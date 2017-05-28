79267468c2fdac9e3da9edf2529281ec877c494b
yuta kitsukawa <ttyk0909@ezweb.ne.jp>
2017-05-28 18:58:43 +0900

    sassにタスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,20 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+var blowserSync = require('browser-sync');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function() {
-  gulp.src('/src/assets/sass/*.scss')
+  gulp.src('src/assets/sass/*.scss')
+  .pipe(plumber({errorHandler: notify.onError('<%- error.mesagge %>')}))
   .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(cssmin())
+  .pipe(rename({suffix: '.min'}))
   .pipe(gulp.dest('dest/assets/css'));
 });
