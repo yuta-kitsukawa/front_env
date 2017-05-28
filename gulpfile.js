6afbf6ecc62caff921bc7659c84265ad0c1e9192
yuta kitsukawa <ttyk0909@ezweb.ne.jp>
2017-05-28 20:12:58 +0900

    gulpにjsタスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
-var blowserSync = require('browser-sync');
+var browserSync = require('browser-sync');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
@@ -19,3 +19,30 @@ gulp.task('sass', function() {
   .pipe(rename({suffix: '.min'}))
   .pipe(gulp.dest('dest/assets/css'));
 });
+
+gulp.task('js', function(){
+  gulp.src('src/assets/js/**/*.js')
+  .pipe(plumber({errorHandler: notify.onError('<%- error.mesagge %>')}))
+  .pipe(uglify())
+  .pipe(rename({suffix:'.min'}))
+  .pipe(gulp.dest('dest/assets/js'))
+});
+
+ gulp.task('html', function(){
+   gulp.src('src/**/*.html')
+   .pipe(gulp.dest('dest/'))
+ });
+
+ gulp.task('reload', function(){
+   browserSync.reload();
+ });
+
+ gulp.task('default', function(){
+   browserSync({
+     server: {baseDir: 'dest'}
+   });
+   gulp.watch('src/**/*.html', ['reload']);
+   gulp.watch('src/**/*.html', ['html']);
+   gulp.watch('src/assets/js/**/*.js',['js']);
+   gulp.watch('src/assets/sass/*.sass',['sass']);
+ });
